cd6a282b84f1a1b865b471137e12ba1b465898e6
Darrell Plank <jar_czar@msn.com>
2017-12-18 08:44:11 -0800

    Interfaces for Rayed configuration

---
+++ MeshNav/TraitInterfaces/IRayed.cs
@@ -0,0 +1,19 @@
+﻿namespace MeshNav.TraitInterfaces
+{
+    // Interface implemented by halfedges
+    public interface IRayed
+    {
+        // A ray extending out from a border vertex
+        bool IsRayed { get; }
+
+        // An edge connecting two rayed edges
+        bool IsInfinite { get; }
+    }
+
+    // Interface implemented by faces
+    public interface IAtInfinity
+    {
+        // This is the face at infinity
+        bool IsFaceAtInfinity { get; }
+    }
+}
